c31bbb4b7df01bdda13fae793661dc8f186a8393
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-04 06:54:26 +0700

    first commit

---
+++ jurnal3_103022330128/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jurnal3_103022330128
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
